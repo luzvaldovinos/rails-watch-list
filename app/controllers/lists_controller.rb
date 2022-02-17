385a6d13327fd3d66690790ce35900ab053b10ea
Luz Valdovinos <luz.valdovinos@gmail.com>
2022-02-17 16:22:07 -0600

    added route, controller, view for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,23 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(review_params)
+    @movie = Movie.find(params[:movie_id])
+    @list.movie = @movie
+  end
+
+
+    private
+
+    def review_params
+      params.require(:bookmark).permit(:list)
+    end
 end
