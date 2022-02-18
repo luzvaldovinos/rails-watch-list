c5feab1c65929f3348071b9f4eb10f7a17e08c91
Luz Valdovinos <luz.valdovinos@gmail.com>
2022-02-18 09:35:46 -0600

    bookmarks and lists working, need to style will do so after deployment

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,16 +8,28 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-  def create
-    @list = List.new(review_params)
-    @movie = Movie.find(params[:movie_id])
-    @list.movie = @movie
+  def new
+    @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
 
     private
 
-    def review_params
-      params.require(:bookmark).permit(:list)
+    def list_params
+      params.require(:list).permit(:name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :create]
+  root to: 'lists#index'
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:create, :new]
+  end
+
+  resources :lists, only: [:destroy]
+  resources :bookmarks, only: [:destroy]
 end
